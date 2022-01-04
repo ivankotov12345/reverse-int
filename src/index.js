57d70a2ba27062e99e14b4d123b85eee290944a0
Ivan Kotov <ivankotov12345@gmail.com>
2022-01-04 15:42:34 +0300

    fix: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n
+    }
+    let number = n.toString();
+  let arrayNumber = number.split('');
+  let reversedArr = arrayNumber.reverse();
+  let numberReversed = reversedArr.join('');
+  return +numberReversed
 }
